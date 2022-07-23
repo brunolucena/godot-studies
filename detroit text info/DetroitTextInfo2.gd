c343990e38e9eba6ad56c9bbdfe1741888b539fd
Bruno Lucena <bruno.lucenac@gmail.com>
2022-07-22 21:46:55 -0300

    DetroitTextInfo2, add prop width

--- detroit text info/DetroitTextInfo2.gd
+++ detroit text info/DetroitTextInfo2.gd
@@ -2,6 +2,10 @@ extends Node2D
 class_name DetroitTextInfo2
 
 export var text: String
+export var width := 250
+
+func _ready() -> void:
+	$Control.rect_size = Vector2(width, $Control.rect_size.y)
 
 func play(_position: Vector2) -> void:
 	position = _position
